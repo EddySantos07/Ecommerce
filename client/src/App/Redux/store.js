930ca844366d4932c90544a80f3cb006795ec428
Eddy <eddy@Eddys-MBP.lan>
2021-05-03 16:22:22 -0400

    added redux dev tools according to docs

--- client/src/App/Redux/store.js
+++ client/src/App/Redux/store.js
@@ -10,12 +10,26 @@ let initialState = {
   ProductStyles: {},
 };
 
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
+  // other store enhancers if any
+);
+
 const store = createStore(
   RootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  // persistedReducer,
+
   initialState,
-  applyMiddleware(thunk)
+  enhancer
 );
+
+// persistedReducer,
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 
 export default store;
