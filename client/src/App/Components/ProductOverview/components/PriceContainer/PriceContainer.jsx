b8ffe8c6d65c85fab90248bfb5beb95d6bb1d594
Eddy <eddy@Eddys-MBP.lan>
2021-05-22 04:05:34 -0400

    finished adding sale / default / regular price calculations

--- client/src/App/Components/ProductOverview/components/PriceContainer/PriceContainer.jsx
+++ client/src/App/Components/ProductOverview/components/PriceContainer/PriceContainer.jsx
@@ -1,27 +1,50 @@
 import React from "react";
 
-
-const filterPrice = ([ProductInfo], ProductStyles, CurrentStyle) => {
-
-
-    console.log(ProductInfo, ProductStyles)
-
-    let price = 0;
-
-    return <h3> {price} </h3>
-
-}
-
-const PriceContainer = ({ productInfo, ProductStyles, CurrentStyleGallery }) => {
-  if (productInfo === undefined || ProductStyles === undefined) {
-    return <div className="PriceContainer">PriceContainer</div>
+const filterPrice = ([ProductInfo], CurrentStyle) => {
+  if (typeof CurrentStyle.sale_price === "number") {
+    console.log(CurrentStyle.sale_price, "in if");
+    return (
+      <div>
+        <h3> Sale Price: {CurrentStyle.sale_price} </h3>
+        <h3>
+          Regular Price:
+          {CurrentStyle.original_price !== undefined ||
+          CurrentStyle.original_price !== null
+            ? CurrentStyle.original_price
+            : ProductInfo.default_price}
+        </h3>
+      </div>
+    );
   }
 
-//   console.log(productInfo[0]);
+  return (
+    <h3>
+      $
+      {CurrentStyle.original_price !== undefined ||
+      CurrentStyle.original_price !== null
+        ? CurrentStyle.original_price
+        : ProductInfo.default_price}
+    </h3>
+  );
+};
+
+const PriceContainer = ({
+  productInfo,
+  ProductStyles,
+  CurrentStyleGallery,
+}) => {
+  if (
+    productInfo === undefined ||
+    ProductStyles === undefined ||
+    Object.keys(CurrentStyleGallery).length === 0
+  ) {
+    return <div className="PriceContainer">PriceContainer</div>;
+  }
+
   return (
     <div className="PriceContainer">
       {" "}
-      {filterPrice(productInfo, ProductStyles, CurrentStyleGallery)}
+      {filterPrice(productInfo, CurrentStyleGallery)}
     </div>
   );
 };
